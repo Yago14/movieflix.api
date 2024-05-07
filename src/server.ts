d44d94e8d31d6360f953f3c400139db852d01804
yago14 <ggbravier@gmail.com>
2024-05-07 12:28:12 -0300

    configuração inicial do prisma

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,7 @@ import  express from "express";
 const port = 3000;
 const app = express();
 
-app.get("/movies ", (req,res)=>{
+app.get("/movies", (req,res)=>{
     res.send("Listagem de filmes")
 
 })
