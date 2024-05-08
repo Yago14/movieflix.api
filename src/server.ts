7ec8cd0e91f18d458286e5b4e45673cb11f781e4
yago14 <ggbravier@gmail.com>
2024-05-08 12:02:46 -0300

    Cadastrando um filme no banco de dados e tratando erro de cadastro

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,8 @@ const port = 3000;
 const app = express();
 const prisma = new PrismaClient();
 
+app.use(express.json());
+
 app.get("/movies", async (_,res)=>{
     const movies = await prisma.movies.findMany({
         orderBy:{
@@ -18,6 +20,31 @@ app.get("/movies", async (_,res)=>{
     res.json(movies);
 
 })
+
+app.post("/movies", async (req,res)=>{
+
+  const { title, genre_id, language_id, oscar_count, release_date} = req.body;
+    
+try{
+
+    await prisma.movies.create({
+        data:{
+            title,
+            genre_id,
+            language_id,
+            oscar_count,
+            release_date: new Date(release_date)
+        }
+    })
+    res.status(201).send();
+
+}catch(error){
+    res.status(500).send({message: "Falha ao cadastrar um filme"})
+}
+
+})
+
+
 
 app.listen(port,()=>{
     console.log(`servidor rodando na pota ${port}`)
