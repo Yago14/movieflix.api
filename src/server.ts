088d3b27210c239ae3d07bc5dbbd09cf4e4374ff
yago14 <ggbravier@gmail.com>
2024-07-04 11:37:01 -0300

    adicionado a filtragem de filmes por genero

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import  express from "express";
 import { PrismaClient } from "@prisma/client";
 
+
 const port = 3000;
 const app = express();
 const prisma = new PrismaClient();
@@ -28,8 +29,7 @@ app.post("/movies", async (req,res)=>{
     
 try{
 
-    const movieWithSameTitle = await prisma.movies.findFirst
-    ({
+    const movieWithSameTitle = await prisma.movies.findFirst({
         where:{title:{equals: title, mode:"insensitive"}}
     })
     if(movieWithSameTitle){
@@ -54,6 +54,60 @@ try{
 }
 
 })
+
+app.put("/movies/:id", async (req, res)=>{
+    
+   
+    const id = Number(req.params.id)
+    const data = {...req.body};
+
+    data.release_date = data.release_date ? new Date(data.release_date) : undefined;
+    
+     await prisma.movies.update({
+        where:{
+            id
+        },
+        data: data
+        
+    });
+    
+    res.status(200).send()
+})
+
+app.delete("/movies/:id", async (req,res)=>{
+    const id = Number(req.params.id);
+
+    const movie = await prisma.movies.findUnique({where: {id}})
+
+    if(!movie){
+        return res.status(404).send({
+            message: "o filme não foi encontrado"
+        })
+    }
+
+    await prisma.movies.delete({where:{id}});
+
+    res.status(200).send();
+  
+})
+
+app.get("/movies/:genreName", async(req,res)=>{
+const moviesFilteredByGenreName = await prisma.movies.findMany({
+    where:{
+        genres:{
+            name:{
+                equals: req.params.genreName,
+                mode: "insensitive"
+            }
+        }
+    }
+})
+
+res.status(200).send(moviesFilteredByGenreName)
+
+})
+
+
 
 
 
